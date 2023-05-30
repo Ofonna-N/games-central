5ecf949a1e9aea348a0d02bd07a324d8df89b523
Ofonna-N <nwezeofonna@gmail.com>
2023-05-30 14:22:22 -0500

    built responsive layout for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Hello o</h1>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <nav className=" bg-orange-500 lg:col-span-2">Nav</nav>
+      <aside className=" bg-red-700 hidden lg:block">Aside</aside>
+      <main className=" bg-emerald-800">Main</main>
+    </div>
+  );
 }
 
 export default App;
