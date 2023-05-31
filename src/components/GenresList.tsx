5154a4aeb99ab0485a6d6211569266068023038b
Ofonna-N <nwezeofonna@gmail.com>
2023-05-31 13:08:32 -0500

    added spinner for genre loading state

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,11 +1,12 @@
 import useGenres from "../hooks/app/useGenres";
+import Spinnner from "./Spinnner";
 
 const GenresList = () => {
-  const { data, errMsg } = useGenres();
+  const { data, isLoading } = useGenres();
 
   return (
     <>
-      <p>{errMsg}</p>
+      {isLoading && <Spinnner />}
       <ul className="text-white text-[2rem] flex flex-col gap-[1rem]">
         {data.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
---
+++ src/components/Spinnner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Spinnner = () => {
+  return (
+    <div className="w-8 h-8 border-white border-solid border-r-transparent animate-spin border-4 rounded-full"></div>
+  );
+};
+
+export default Spinnner;
