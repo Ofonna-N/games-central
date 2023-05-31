91356bf4ef51bc79f060ab62f206fb20782b6aec
Ofonna-N <nwezeofonna@gmail.com>
2023-05-30 19:39:53 -0500

    deleted theme store and slice, using local storage instead

--- src/hooks/storehooks/useStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import createThemeSlice, { ThemeSlice } from "./createThemeSlice";
-
-const useStore = create<ThemeSlice>((...a) => ({
-  ...createThemeSlice(...a),
-}));
-
-export default useStore;
