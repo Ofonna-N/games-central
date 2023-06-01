0989e754dee8143a3488026198f3c9d934b8e4d1
Ofonna-N <nwezeofonna@gmail.com>
2023-06-01 17:23:12 -0500

    regactored games grid

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -11,23 +11,21 @@ type Props = {
 const GamesGrid = ({ gameQuery }: Props) => {
   const { data, errMsg, isLoading } = useGames(gameQuery);
   // console.log(selectedPlatform);
+  if (errMsg) return <p>{errMsg}</p>;
   return (
-    <>
-      <p>{errMsg}</p>
-      <ul className="grid grid-cols-1 gap-[2rem] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {isLoading &&
-          Array.from({ length: 20 }).map((_, i) => (
-            <li key={i}>
-              <GameCardSkeleton />
-            </li>
-          ))}
-        {data.map((game) => (
-          <li key={game.id}>
-            <GameCard game={game} />
+    <ul className="grid grid-cols-1 gap-[2rem] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {isLoading &&
+        Array.from({ length: 20 }).map((_, i) => (
+          <li key={i}>
+            <GameCardSkeleton />
           </li>
         ))}
-      </ul>
-    </>
+      {data.map((game) => (
+        <li key={game.id}>
+          <GameCard game={game} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
