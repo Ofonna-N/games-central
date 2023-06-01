4e495eb07cfc4717c0bfffb24034ea360ccbe588
Ofonna-N <nwezeofonna@gmail.com>
2023-06-01 10:12:36 -0500

    added bright secondary color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
       colors: {
         "dark-primary": "#151515",
         "dark-secondary": "#202020",
+        "dark-secondary-bright": "#333333",
       },
       gridTemplateColumns: {
         "side-main": "220px minmax(0, 1fr)",
