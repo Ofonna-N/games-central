37e4c1d2632617352569ec2ebc50d73fa6ab5534
Ofonna-N <nwezeofonna@gmail.com>
2023-06-06 19:15:29 -0500

    Created Resuable API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,27 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+export type FetchResponse<T> = {
+  count: number;
+  results: T[];
+};
+
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: import.meta.env.VITE_RAWG_APIKEY,
   },
 });
+
+export default class APIClient<T> {
+  private endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
